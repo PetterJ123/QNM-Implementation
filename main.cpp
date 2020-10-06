7404f747e3ae43c22ad29807b8c755deaffa4179
PetterJ123 <mr.pepe999@gmail.com>
2020-10-06 16:02:49 +0200

    Removed old code and added new main file

--- main.cpp
+++ main.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
-#include <queue>
 #include <string>
-#include "CPU.hpp"
+#include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	CPU cpu = CPU();
-	float reqData = 44.4123;
-	// Take in a filename to download
-	cpu.HandleFetchData(reqData);
+int main(int argc, char *argv[])
+{
+	if((std::string)argv[1] == "--help")
+	{
+		std::cout << "<service demand> <think time> <customers>\n";
+		exit(0);
+	}
+	double Qi[100];
+	double Rprime[100];
+	double X[100];
 
-	// Place file-download-request in a queue for the CPU
+	int n = 50;     // # of customers
 
-	// After an specific amount of time the CPU will handele the request
+	double Di = 0.25;   // service demand
+	double Z = 3.0;     // think time
 
-	// After the request is handled, ask one disk for the data
-		// If the disk don't have the data, return 0 (or something)
-		// The disk will ask the next disk for the data until the data is found
-	
-	// After the CPU have recieved the data send it back to the user (print it out)
-	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	if (argc > 1)
+	{
+		Di = strtod(argv[1], &argv[1]);
+		Z = strtod(argv[2], &argv[2]);
+		n = strtod(argv[3], &argv[3]);
+		std::cout << "Using following values: " << "\nthink time (Z): " << Z << "\nService demand(D): " << Di << "\nCustomers/Requests (n): " << n << "\n\n";
+	}
 
-	std::cout << "Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n";
+	Qi[0] = 0.0;
+
+	for (int i=1; i<n; i++){
+		Rprime[i] = Di*(1.0 + Qi[i - 1]);
+		X[i] = i/(Z + Rprime[i]);
+		Qi[i] = X[i]*Rprime[i];
+
+		double Ui = X[i] * Di * 100;
+		double Ri = Di/(1.0 - Ui * 0.01);
+
+		std::cout << "Customer=" << i << "\tUtilization=" << Ui << "%\tResidence time=" << Rprime[i] << "\tResponse time= " << Ri << "\tThroughput=" << X[i] << "\tQueue=" << Qi[i] << "\n\n";
+	}
+
 	return 0;
 }
